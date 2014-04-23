92a0f4417c5550eef8709ae53f94d1e2f1d5082a
Renato Biancalana da Silva <r.silva@pleimo.com>
2014-04-23 09:15:33 -0300

    aeaesalçkjfdalçkfj

--- application/views/home.php
+++ application/views/home.php
@@ -6,29 +6,23 @@
         </div>
     </div>
     <hr>
+
     <div class="row">
         <div class="small-4 medium-4 large-4 columns" style="background-color: #f3f3f3; border: 1px solid #d6d6d6; border-radius: 3px; padding: 5px;">
             <div class="row collapse">
+                <ul class="no-bullet">
+                    <li ng-repeat="time in times">
+                        {{time.nome}}
+                    </li>
+                </ul>
                 <div class="small-10 medium-10 large-10 columns">
-                    <input type="text" ng-model="qtdTimes" placeholder="Quantidade de times">
+                    <input type="text" ng-model="novoTime" placeholder="Nome do time">
                 </div>
                 <div class="small-2 medium-2 large-2 columns">
-                    <a href="javascript:void(0);" ng-click="startCopinha()" class="button postfix">Salvar</a>
+                    <a href="javascript:void(0);" ng-click="addTimes()" class="button postfix">Salvar</a>
                 </div>
             </div>
         </div>
 
-        <div class="small-6 medium-6 large-6 small-offset-2 medium-offset-2 large-offset-2" style="background-color: #f3f3f3; border: 1px solid #d6d6d6; border-radius: 3px; padding: 5px;">
-
-        </div>
-    </div>
-
-    <hr>
-
-    <div class="row">
-        <div class="small-4 medium-4 large-4 columns" style="background-color: #f3f3f3; border: 1px solid #d6d6d6; border-radius: 3px; padding: 5px;">
-            <h4>Novos times</h4>
-            <input type="text" ng-repeat="novotime in novostimes" id="time-{{novotime.key}}" name="time-{{novotime.key}}" placeholder="Nome do time...">
-        </div>
     </div>
 </div>
--- template/js/home.js
+++ template/js/home.js
@@ -1,16 +1,9 @@
 function HomeController($scope) {
-    $scope.novostimes = [];
     $scope.times = [];
 
-    $scope.startCopinha = function () {
-        var $total = $scope.qtdTimes;
-        for (var i = 0; i < $total; i++){
-            $scope.novostimes.push({key:i});
-        }
-    };
-
-    $scope.addTime = function () {
-
+    $scope.addTimes = function () {
+        $scope.times.push({nome:$scope.novoTime});
+        $scope.novoTime = '';
     };
 
     $scope.rmTime = function () {
